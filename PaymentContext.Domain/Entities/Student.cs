1b51921a76ec7ca36a5d9d2df44d076e3e9ef9f2
zetunejacques <jacques.zetune@inetsystem.com.br>
2021-08-20 18:05:18 -0300

    estruturaçãso da Solução, Entities, ValueObjects

---
+++ PaymentContext.Domain/Entities/Student.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Linq;
+using PaymentContext.Domain.ValueObjects;
+using PaymentContext.Shared.Entities;
+
+namespace PaymentContext.Domain.Entities
+{
+    public class Student : Entity
+    {
+        private IList<Subscription> _subscriptions;
+        public Student(Name name, Document document, Email email)
+        {
+            Name = name;
+            Document = document;
+            Email = email;
+            _subscriptions = new List<Subscription>();
+        }
+
+        public Name Name { get; private set; }
+
+        public Document Document { get; private set; }
+        public Email Email { get; private set; }
+        public Address Address { get; private set; }
+        public IReadOnlyCollection<Subscription> Subscriptions { get { return _subscriptions.ToArray(); } }
+
+        public void AddSubscription(Subscription subscription)
+        {
+            foreach (var sub in Subscriptions)
+                sub.Inactivate();
+            _subscriptions.Add(subscription);
+        }
+
+    }
+
+}
---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            //var student = new Student();
+        }
+    }
+
+}
